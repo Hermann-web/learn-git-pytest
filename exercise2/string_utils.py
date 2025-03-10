30466c8beb04e33a19603a4031aec2d9f3b037b3
grinboja <benaddihamza192@gmail.com>
2025-03-10 16:20:27 +0100

    exo 1 hamza Ben-addi

--- exercise2/string_utils.py
+++ exercise2/string_utils.py
@@ -12,7 +12,7 @@ def reverse_string(s: str) -> str:
         The reversed string
     """
     # TODO: Implement this function
-    pass
+    return s[::-1] # méthode le slicing
 
 
 def count_vowels(s: str) -> int:
@@ -27,7 +27,14 @@ def count_vowels(s: str) -> int:
         The number of vowels in the string
     """
     # TODO: Implement this function
-    pass
+    vowels = {'a', 'e', 'i', 'o', 'u'} # Définir l'ensemble des voyelles
+    s = s.lower() # Convertir la chaîne en minuscules
+    total_vowels = 0
+    for char in s:
+        if char in vowels:
+            total_vowels += 1  # Incrémenter si le caractère est une voyelle
+            
+    return total_vowels
 
 
 def is_palindrome(s: str) -> bool:
@@ -43,7 +50,12 @@ def is_palindrome(s: str) -> bool:
         True if the string is a palindrome, False otherwise
     """
     # TODO: Implement this function
-    pass
+
+     # Convertir la chaîne en minuscules et supprimer les espaces
+    
+    s_no_spaces = s.replace(" ", "")
+    cleaned_s = s_no_spaces.lower()
+    return cleaned_s == cleaned_s[::-1]
 
 
 def capitalize_words(s: str) -> str:
@@ -57,4 +69,5 @@ def capitalize_words(s: str) -> str:
         The input string with the first letter of each word capitalized
     """
     # TODO: Implement this function
-    pass
+    return s.title()
+
